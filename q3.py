b54b32e724f9d4997b7cbb639c22661037332251
ItayHasidi <itayhasidi@gmail.com>
2022-11-01 21:37:55 +0200

    added a helper function to q3
    still untested

--- q3.py
+++ q3.py
@@ -1,22 +1,92 @@
 from pydoc import locate
+list_type = locate("list")
+tuple_type = locate("tuple")
+set_type = locate("set")
+dict_type = locate("dict")
+types = {list_type, tuple_type, set_type, dict_type}
+
+
+def rec_sort(data):
+    """
+    Gets any kind of data and converts whatever is not a dataset into string.
+    All datasets are also turned into strings (deep iterative).
+    Afterwards sorts and returns.
+    """
+    if type(data) == list_type:
+        new_data: list = []
+        for item in data:
+            if type(item) in types:
+                item = rec_sort(item)
+            else:
+                item = str(item)
+            new_data.append(item)
+        data = new_data
+        data.sort()
+
+    elif type(data) == tuple_type or type(data) == set_type or type(data) == dict_type:
+        if type(data) == tuple_type or type(data) == set_type:
+            new_data: list = []
+            for item in data:
+                if type(item) in types:
+                    item = rec_sort(item)
+                else:
+                    item = str(item)
+                new_data.append(item)
+            new_data.sort()
+            if type(data) == tuple_type:
+                data = tuple(new_data)
+            else:
+                data = list(new_data)
+
+        else:
+            new_lst: dict = {}
+            for item_key in data:
+                if type(data[item_key]) in types:
+                    data[item_key] = rec_sort(data[item_key])
+                # else:
+                item_val = data[item_key]
+                item_key = str(item_key)
+                new_lst[item_key] = item_val
+            # print(new_lst)
+            # data = dict(new_lst)
+            data = dict(sorted(new_lst.items()))
+
+    return data
 
 
 def print_sorted(lst):
-    types = {locate("list"), locate("tuple"), locate("set"), locate("dict")}
-    types_sort = {locate("list"), locate("tuple"), locate("set")}
+
+    types_sort = {list_type, tuple_type}
     if type(lst) not in types:
         print(lst)
     else:
+        # new_lst: dict = {}
+        # new_lst.clear()
+        # print(new_lst, lst)
+
+        # for item_key in lst:
+        #     item_val = lst[item_key]
+        #     # lst.pop(item_key)
+        #     item_key = str(item_key)
+        #     new_lst[item_key] = item_val
+        # print(new_lst, lst)
+        # lst = new_lst
         for item in lst:
+            item = str(item)
             if type(lst[item]) in types_sort:
                 lst[item].sort()
-            elif type(lst[item]) == locate("dict"):
+            elif type(lst[item]) == dict_type:
                 lst[item] = sorted(lst[item].items())
                 # print(sorted(lst[item].items()))
+            # else:
+            #     lst[item] = str(lst[item])
+            #     print(type(lst[item]), lst[item])
         lst = sorted(lst.items())
         print(lst)
 
 
 if __name__ == '__main__':
     x = {"a": 5, "c": 6, "b": [1, 3, 2, 4]}
-    print_sorted(x)  # prints e.g. {"a":5, "b":[1,2,3,4], "c":6}
+    # print_sorted(x)  # prints e.g. {"a":5, "b":[1,2,3,4], "c":6}
+    x = {"a": 5, 1: 6, "b": [1, 3, 2, 4], 0: "a", "2": {"b", "c", 1, 0, -1, "A"}}
+    print(rec_sort(x))  # prints e.g. {"a":5, "b":[1,2,3,4], "c":6}
