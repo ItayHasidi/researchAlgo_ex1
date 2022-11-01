5c3b332c8e5e9d7fa74fd12183cc9c6303651b44
ItayHasidi <itayhasidi@gmail.com>
2022-11-01 16:06:31 +0200

    q2 working
    no tests

--- q2.py
+++ q2.py
@@ -16,10 +16,19 @@ def breadth_first_search(start, end, neighbor_function):
         p.append(v)
         for n in neighbor_function(v):
             if n == end:
-                return p
-            q.append(n)
+                p.append(n)
+                end_path = [n]
+                last_v = n
+                while len(p) > 0:
+                    temp_v = p.pop(len(p) - 1)
+                    if temp_v in neighbor_function(last_v):
+                        end_path.insert(0, temp_v)
+                        last_v = temp_v
+                return end_path
+            if n not in q:
+                q.append(n)
     return False
 
 
 if __name__ == '__main__':
-    breadth_first_search(start=(0, 0), end=(2, 2), neighbor_function=four_neighbor_function)
+    print(breadth_first_search(start=(0, 0), end=(2, 2), neighbor_function=four_neighbor_function))
